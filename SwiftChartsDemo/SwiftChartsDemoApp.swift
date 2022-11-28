9f216bd00a426dc782f4f19e187de192eae16ed8
jesslemon <jessicalemon@churchofjesuschrist.org>
2022-11-28 09:29:04 -0700

    Initial Commit

---
+++ SwiftChartsDemo/SwiftChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsDemoApp.swift
+//  SwiftChartsDemo
+//
+//  Created by Jessica Lemon on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
